210dd017474e5677f6f87d6a12c5801533ef6c1e
aavetisyanit <aavetisyanit@gmail.com>
2021-03-24 09:59:54 -0500

    Install expo app

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<Text>Testing</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
